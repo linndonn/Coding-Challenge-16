7a9307789e98f336b82c49d5a64e0542fb4be8f7
linndonn <linndonn@usf.edu>
2025-03-29 17:46:08 -0400

    Task 3 : Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -22,6 +22,30 @@ function fetchProductsThen() {
         });
     }
 
+
     //Task 3: Fetch Products with async/await
 
-    
+    // Fetches product data from a given API using async/await.
+    async function fetchProductsAsync() {
+        // Fetches product data from a given API.
+  try {
+    const response = await fetch("https://www.course-api.com/javascript-store-products");
+    // Check if the response is ok.
+    if (!response.ok) {
+        // If the response is not right, produce an error
+      throw new Error("Failed to fetch");
+    }
+    const products = await response.json();
+    // Loop through the products and log the name of each product
+    displayProducts(products);
+} catch (error) {
+    handleError(error);
+}}
+//Develop the helper function to render products to the page
+function displayProducts(products) {  
+    products.forEach(product => { 
+        const productElement = document.createElement('div'); 
+        productElement.classList.add('product'); 
+        productElement.textContent = `${product.fields.name} - $${product.fields.price}`;  
+    });
+}
